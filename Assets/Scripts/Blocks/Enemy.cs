c6ff8abe20e2ae6797e29f56a23d43641aa9a47c
Tony Cabaye <tonai59+github@gmail.com>
2021-10-23 22:45:07 +0200

    feat: add ability to destroy enemies

--- Assets/Scripts/Blocks/Enemy.cs
+++ Assets/Scripts/Blocks/Enemy.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Threading.Tasks;
+using UnityEngine;
 
 public class Enemy : Block
 {
+    private bool isDestroyed = false;
+
     public Task CheckShoot(Direction direction)
     {
-        if (direction == DirectionHelper.GetOppositeDirection(this.direction)) {
+        if (direction == DirectionHelper.GetOppositeDirection(this.direction) && !isDestroyed) {
             Shooter shooter = GetComponent<Shooter>();
             if (shooter != null)
             {
@@ -14,5 +18,43 @@ public class Enemy : Block
             }
         }
         return null;
+    }
+
+    public override Vector3 GetShootHitPosition(float yOffset, ref Direction direction, ref bool continueShooting)
+    {
+        if (direction == DirectionHelper.GetOppositeDirection(this.direction))
+        {
+            continueShooting = false;
+            return new Vector3(position.x * boardManager.tileSize, yOffset, position.y * boardManager.tileSize);
+        }
+
+        return base.GetShootHitPosition(yOffset, ref direction, ref continueShooting);
+    }
+
+    public override bool ShootThrough(GameObject gameObject, Direction direction, Shooter shooter, Action callback)
+    {
+        if (direction == DirectionHelper.GetOppositeDirection(this.direction))
+        {
+            isDestroyed = true;
+            ApplyColor(ComponentHelper.FindComponentInChildWithTag<Transform>(this.gameObject, "Tank"));
+            shooter.DestroyLaser();
+            callback();
+            return true;
+        }
+
+        return base.ShootThrough(gameObject, direction, shooter, callback);
+    }
+
+    private void ApplyColor(Transform transform)
+    {
+        Renderer renderer = transform.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = Color.black;
+            Transform[] t = renderer.gameObject.GetComponentsInChildren<Transform>();
+            if (t.Length > 1) {
+                ApplyColor(t[1]);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Helpers/ComponentHelper.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public static class ComponentHelper
+{
+    public static T FindComponentInChildWithTag<T>(this GameObject parent, string tag) where T : Component
+    {
+        Transform t = parent.transform;
+        foreach (Transform tr in t)
+        {
+            if (tr.tag == tag)
+            {
+                return tr.GetComponent<T>();
+            }
+        }
+        return null;
+    }
+}
